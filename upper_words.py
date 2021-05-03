f8fb4789538387638923fd7e63bab0cceb6a3000
rafael <rafaelpaula15@hotmail.com>
2021-05-03 16:22:05 -0400

    updating file

--- upper_words.py
+++ upper_words.py
@@ -1,8 +1,6 @@
 def upper_words(words):
     """This function will change str to uppercase 
-    and word must start with e or h"""
-    
+    and word must start with h or e"""
     for word in words:
-        if word[0] != "e" and word[0] != "h":
-            return "WORDS MUST START WITH E OR H"
-        print(word.upper())
+        if word[0] == "h" or word[0] == "e":
+            print(word.upper())
